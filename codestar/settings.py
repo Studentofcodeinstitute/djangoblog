776edd8869ce671c209ae2e1c6ea5689e3f13372
baijid Rahman <baijidrahman12@gmail.com>
2024-02-12 21:29:16 +0000

    Changed allowed hosts link

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["8000-studentofcod-djangoblog-pe15e0np6k9.ws-eu108.gitpod.io"]
 
 
 
