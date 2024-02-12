d03cd93ce6ea655b27ff01546771cb7f67acdef8
baijid Rahman <baijidrahman12@gmail.com>
2024-02-12 22:34:09 +0000

    Add some direction in settings.py file

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,9 +30,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-studentofcod-djangoblog-pe15e0np6k9.ws-eu108.gitpod.io"]
+if True:
+    ALLOWED_HOSTS = ['8000-studentofco-cifsfhellod-6npmt4dsa3d.ws-eu108.gitpod.io']
 
-
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
@@ -82,16 +84,19 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+
+if True:
+   DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
+else:
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    DATABASES = {
+    'default': dj_database_url.config(),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
