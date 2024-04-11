10b2a014f443ca6839bfd9dff454124ff07ab185
baijid Rahman <baijidrahman12@gmail.com>
2024-04-11 11:14:41 +0000

    Add database url

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,8 +31,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
+# DEBUG = os.environ.get('DEBUG')
+
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -112,17 +113,21 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if 'DATABASE_URL' in os.environ:
-  DATABASES = {
+#if 'DATABASE_URL' in os.environ:
+  #DATABASES = {
+    #'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#}
+#else:
+    #DATABASES = {
+      #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+       #}
+    #}
+
+DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-else:
-    DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-       }
-    }
 
 
 CSRF_TRUSTED_ORIGINS = [
